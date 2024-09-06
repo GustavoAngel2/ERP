e6618c0c0404c733047366ab35ba4c182b5b9887
GustavoAngel2 <12755@utl.edu.mx>
2024-09-06 13:03:31 -0600

    -
    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component,OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {FormsModule} from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -19,7 +21,9 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatInputModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatSidenavModule,
     FormsModule,
+    NgIf,
     RouterLink
   ],
   templateUrl: './app.component.html',
@@ -27,11 +31,21 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 })
 
 export class AppComponent implements OnInit{
-  title = 'ERP';
+  currentUrl: string = '';
 
+  constructor(private router: Router, private route: ActivatedRoute) {}
   value = 'clear me'
 
-  ngOnInit(): void {
+  ngOnInit() {
+    // Obtener la URL actual directamente
+    this.currentUrl = this.router.url;
 
+    // Suscribirse a los cambios de la ruta
+    this.router.events.subscribe((event) => {
+      if (event.constructor.name === 'NavigationEnd') {
+        this.currentUrl = this.router.url;
+        console.log('Ruta actual:', this.currentUrl);
+      }
+    });
   }
 }
